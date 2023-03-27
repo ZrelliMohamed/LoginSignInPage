dae0d801995fef8bd0ffd0034fda683f89dd1ba9
Med <hamma-01@live.fr>
2023-03-27 16:48:42 +0100

    Essai with regular expression on Email

--- login And Sign In Page with React/FrontEnd/src/component/LoginPage.jsx
+++ login And Sign In Page with React/FrontEnd/src/component/LoginPage.jsx
@@ -1,37 +1,42 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from "react-router-dom";
 import axios from "axios"
-const LoginPage = ({getUser}) => {
-    const navigate = useNavigate();
-    const [msg,setMsg]= useState('')
-    // const [Email,setEmail]= useState('')
-    // const [password,setPassword]= useState('')
-    const [data,setdata]= useState([])
-    const [user,setUser]=useState({email:'',password:''})
-     const handelSubmit = (e)=>{
-      e.preventDefault();
-     
-      axios.get(`http://localhost:8000/api/user/${user.email}/${user.password}`).then(({data})=>{
-        setdata(data)
-        if(data.length){ navigate("/HomePage"); getUser(user)}
-        else setMsg('User Not Found')
-    }).catch((err)=>{
-        setMsg('User Not Found')
-      
+const LoginPage = ({ getUser }) => {
+  const navigate = useNavigate();
+  const [msg, setMsg] = useState('')
+  // const [Email,setEmail]= useState('')
+  // const [password,setPassword]= useState('')
+  const [data, setdata] = useState([])
+  const [user, setUser] = useState({ email: '', password: '' })
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    axios.get(`http://localhost:8000/api/user/${user.email}/${user.password}`).then(({ data }) => {
+      setdata(data)
+      if (data.length) { navigate("/HomePage"); getUser(user) }
+      else setMsg('User Not Found')
+    }).catch((err) => {
+      setMsg('User Not Found')
+
     })
-}
-    // console.log(data);
+  }
+   const ifVAlidate =(e)=>{
+    setUser({ ...user, email: e.target.value })
+    setMsg('')
+   }
+   console.log(user);
   return (
     <div>
       <form onSubmit={handelSubmit} >
         <p>{msg}</p><br />
-       <input type="email"  placeholder='Email'  onChange={(e)=>{setUser({...user,email:e.target.value})}} required/>
-       <input type="password"  placeholder='Password'  onChange={(e)=>{setUser({...user,password:e.target.value})}} required/>
-       {/* <input type="text"  placeholder='Email'  onChange={(e)=>{setEmail(e.target.value)}}/>
+        <input type="email" placeholder='Email' onChange={(e) => { 
+          /^[a-z0-9.-_]+@[a-z0-9.-_]+\.[a-z]{1,8}$/i.test(e.target.value)? ifVAlidate(e) : setMsg("Email not Valide")
+          }} required />
+        <input type="password" placeholder='Password' onChange={(e) => { setUser({ ...user, password: e.target.value }) }} required />
+        {/* <input type="text"  placeholder='Email'  onChange={(e)=>{setEmail(e.target.value)}}/>
        <input type="text"  placeholder='Password'  onChange={(e)=>{setPassword(e.target.value)}}/> */}
-       <input type="submit"  value='Login' />
-       <input type="button" value="SignIn"  onClick={()=>{navigate("/SignIn")}}/>
-       </form>
+        <input type="submit" value='Login' />
+        <input type="button" value="SignIn" onClick={() => { navigate("/SignIn") }} />
+      </form>
     </div>
   )
 }
--- login And Sign In Page with React/FrontEnd/src/component/SignInPage.jsx
+++ login And Sign In Page with React/FrontEnd/src/component/SignInPage.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import { useNavigate } from "react-router-dom";
 const SignInPage = ( {getUser}) => {
   const navigate = useNavigate()
+  const [FormValidation,setFormValidation] =useState(false)
   const [msg,setMsg]= useState('')
   const [user,setUser] = useState({
       email: "",
@@ -10,22 +11,38 @@ const SignInPage = ( {getUser}) => {
       user_name: "",
       user_phone: 0  
   })
-  const handelSubmit = ()=>{
-    e.preventDefault();
-    axios.post("http://localhost:8000/api/user/addOne",user).then((result)=>{ navigate('/HomePage');getUser(user);})
-          .catch((err)=>{setMsg('User Not Created')})
+  const handelSubmit = (e)=>{
+    e.preventDefault()
+    axios.post("http://localhost:8000/api/user/addOne",user)
+    .then((result)=>{ navigate('/HomePage');getUser(user);})
+    .catch((err)=>{setMsg('User Not Created')})
   }
+
+  const ifVAlidateEmail =(e)=>{
+    setUser({ ...user, email: e.target.value })
+    setMsg('')
+    setFormValidation(true)
+   }
+   const ifNotVAlidateEmail =()=>{
+    setMsg('Email Not Validate')
+    setFormValidation(false)
+   }
+   console.log(FormValidation,user,msg);
   return (
     <div>
       <p>
         {msg}
       </p>
-      <form onSubmit={handelSubmit}>
-        <input type="email" placeholder='Email'  onChange={(e)=>{setUser({...user,email:e.target.value})}} required/><br />
-        <input type="password" placeholder='Password'  onChange={(e)=>{setUser({...user,password:e.target.value})}}required/><br />
+      <form onSubmit={FormValidation ?handelSubmit : (e)=>{
+       e.preventDefault()
+        setMsg('FORM INVALID')}}>
+        <input type="email" placeholder='Email'  onChange={(e) => { 
+          /^[a-z0-9.-_]+@[a-z0-9.-_]+\.[a-z]{1,8}$/i.test(e.target.value)? ifVAlidateEmail(e) : ifNotVAlidateEmail()
+          }} required /><br />
+        <input type="text" placeholder='Password'  onChange={(e)=>{setUser({...user,password:e.target.value})}}required/><br />
         <input type="text" placeholder='NickName' onChange={(e)=>{setUser({...user,user_name:e.target.value})}}required/><br />
         <input type="number" placeholder='Phone Number' onChange={(e)=>{setUser({...user,user_phone:e.target.value})}}required/><br />
-        <input type="submit" value="Sign IN"/>
+        <input type="submit" value="Sign IN"  />
         <input type="button" value="Login" onClick={()=>{ navigate('/')}} />
         </form>
     </div>
